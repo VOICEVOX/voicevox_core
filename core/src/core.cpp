04cc45f78c03d648d0040f584aca7428124e450b
Oyaki <oc.yaki122@gmail.com>
2021-12-07 14:59:01 +0900

    とりあえず実装

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -15,6 +15,8 @@
 #define ONNX_ERR "ONNX raise exception: "
 #define GPU_NOT_SUPPORTED_ERR "This library is CPU version. GPU is not supported."
 
+constexpr float PHONEME_LENGTH_MINIVAL = 0.01f;
+
 namespace fs = std::filesystem;
 constexpr std::array<int64_t, 0> scalar_shape{};
 constexpr std::array<int64_t, 1> speaker_shape{1};
@@ -22,16 +24,10 @@ constexpr std::array<int64_t, 1> speaker_shape{1};
 static std::string error_message;
 static bool initialized = false;
 
-bool open_models(
-    const fs::path& yukarin_s_path,
-    const fs::path& yukarin_sa_path,
-    const fs::path& decode_path,
-    std::vector<unsigned char>& yukarin_s_model,
-    std::vector<unsigned char>& yukarin_sa_model,
-    std::vector<unsigned char>& decode_model
-) {
-  std::ifstream yukarin_s_file(yukarin_s_path, std::ios::binary),
-      yukarin_sa_file(yukarin_sa_path, std::ios::binary),
+bool open_models(const fs::path &yukarin_s_path, const fs::path &yukarin_sa_path, const fs::path &decode_path,
+                 std::vector<unsigned char> &yukarin_s_model, std::vector<unsigned char> &yukarin_sa_model,
+                 std::vector<unsigned char> &decode_model) {
+  std::ifstream yukarin_s_file(yukarin_s_path, std::ios::binary), yukarin_sa_file(yukarin_sa_path, std::ios::binary),
       decode_file(decode_path, std::ios::binary);
   if (!yukarin_s_file.is_open() || !yukarin_sa_file.is_open() || !decode_file.is_open()) {
     error_message = FAILED_TO_OPEN_MODEL_ERR;
@@ -49,20 +45,16 @@ struct Status {
       : root_dir_path(root_dir_path_utf8),
         use_gpu(use_gpu_),
         memory_info(Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU)),
-        yukarin_s(nullptr), yukarin_sa(nullptr), decode(nullptr) {}
+        yukarin_s(nullptr),
+        yukarin_sa(nullptr),
+        decode(nullptr) {}
 
   bool load() {
     // deprecated in C++20; Use char8_t for utf-8 char in the future.
     fs::path root = fs::u8path(root_dir_path);
     std::vector<unsigned char> yukarin_s_model, yukarin_sa_model, decode_model;
-    if (!open_models(
-        root / "yukarin_s.onnx",
-        root / "yukarin_sa.onnx",
-        root / "decode.onnx",
-        yukarin_s_model,
-        yukarin_sa_model,
-        decode_model
-    )) {
+    if (!open_models(root / "yukarin_s.onnx", root / "yukarin_sa.onnx", root / "decode.onnx", yukarin_s_model,
+                     yukarin_sa_model, decode_model)) {
       return false;
     }
     Ort::SessionOptions session_options;
@@ -152,7 +144,11 @@ bool yukarin_s_forward(int length, long *phoneme_list, long *speaker_id, float *
     Ort::Value output_tensor = to_tensor(output, phoneme_shape);
 
     status->yukarin_s.Run(Ort::RunOptions{nullptr}, inputs, input_tensors.data(), input_tensors.size(), outputs,
-                           &output_tensor, 1);
+                          &output_tensor, 1);
+
+    for (int i = 0; i < length; i++) {
+      if (output[i] < PHONEME_LENGTH_MINIVAL) output[i] = PHONEME_LENGTH_MINIVAL;
+    }
   } catch (const Ort::Exception &e) {
     error_message = ONNX_ERR;
     error_message += e.what();
@@ -189,7 +185,7 @@ bool yukarin_sa_forward(int length, long *vowel_phoneme_list, long *consonant_ph
     Ort::Value output_tensor = to_tensor(output, phoneme_shape);
 
     status->yukarin_sa.Run(Ort::RunOptions{nullptr}, inputs, input_tensors.data(), input_tensors.size(), outputs,
-                            &output_tensor, 1);
+                           &output_tensor, 1);
   } catch (const Ort::Exception &e) {
     error_message = ONNX_ERR;
     error_message += e.what();
@@ -215,7 +211,7 @@ bool decode_forward(int length, int phoneme_size, float *f0, float *phoneme, lon
     Ort::Value output_tensor = to_tensor(output, wave_shape);
 
     status->decode.Run(Ort::RunOptions{nullptr}, inputs, input_tensor.data(), input_tensor.size(), outputs,
-                        &output_tensor, 1);
+                       &output_tensor, 1);
   } catch (const Ort::Exception &e) {
     error_message = ONNX_ERR;
     error_message += e.what();
