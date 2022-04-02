b87a61656d4249b5cd69c9e9e4f9f9dae9633979
shigobu <shigobu@shigobu.com>
2022-04-02 12:05:55 +0900

    音声生成と再生の実装

--- example/cpp/windows/simple_tts/simple_tts.cpp
+++ example/cpp/windows/simple_tts/simple_tts.cpp
@@ -25,40 +25,59 @@ int main() {
   std::wcout << L">";
   std::wstring speak_words;
   std::wcin >> speak_words;
-  std::wcout << speak_words << std::endl;
 
   // coreの初期化
   initialize(false);
 
   VoicevoxResultCode result = VoicevoxResultCode::VOICEVOX_RESULT_SUCCEED;
 
-  // openjtalk辞書の読み込み
+  std::wcout << L"openjtalk辞書の読み込み" << std::endl;
   result = voicevox_load_openjtalk_dict(GetOpenJTalkDict().c_str());
   if (result != VoicevoxResultCode::VOICEVOX_RESULT_SUCCEED) {
     std::cout << voicevox_error_result_to_message(result) << std::endl;
     return 0;
   }
+
+  std::wcout << L"音声生成中" << std::endl;
+  int64_t speaker_id = 0;
+  int output_binary_size = 0;
+  uint8_t* output_wav = nullptr;
+  result = voicevox_tts(wide_to_utf8_cppapi(speak_words).c_str(), speaker_id, &output_binary_size, &output_wav);
+  if (result != VoicevoxResultCode::VOICEVOX_RESULT_SUCCEED) {
+    std::cout << voicevox_error_result_to_message(result) << std::endl;
+    return 0;
+  }
+
+  std::wcout << L"音声再生中" << std::endl;
+  PlaySound((LPCTSTR)output_wav, nullptr, SND_MEMORY);
+
 }
 
-// OpenJTalk辞書のパスを取得します。
+/// <summary>
+/// OpenJTalk辞書のパスを取得します。
+/// </summary>
+/// <returns>OpenJTalk辞書のパス</returns>
 std::string GetOpenJTalkDict() {
   wchar_t buff[MAX_PATH] = {0};
   PathCchCombine(buff, MAX_PATH, GetExeDirectory().c_str(), OPENJTALK_DICT_NAME);
   std::string retVal = wide_to_multi_capi(buff);
-  // std::string retVal((char*)buff);
-
-  // ConvU32ToU8((char32_t*)buff, retVal);
   return retVal;
 }
 
-//自分自身のあるパスを取得する
+/// <summary>
+/// 自分自身のあるパスを取得する
+/// </summary>
+/// <returns>自分のexeのフルパス</returns>
 std::wstring GetExePath() {
   wchar_t buff[MAX_PATH] = {0};
   GetModuleFileName(nullptr, buff, MAX_PATH);
   return std::wstring(buff);
 }
 
-//自分自身のあるディレクトリを取得する
+/// <summary>
+/// 自分自身のあるディレクトリを取得する
+/// </summary>
+/// <returns>自分のexeのあるディレクトリ</returns>
 std::wstring GetExeDirectory() {
   wchar_t buff[MAX_PATH] = {0};
   wcscpy_s(buff, MAX_PATH, GetExePath().c_str());
@@ -67,8 +86,14 @@ std::wstring GetExeDirectory() {
   return std::wstring(buff);
 }
 
-//ワイド文字をShift_JISに変換します。
-// https://nekko1119.hatenablog.com/entry/2017/01/02/054629 から引用
+/// <summary>
+/// ワイド文字列をShift_JISに変換します。
+/// </summary>
+/// <param name="src">ワイド文字列</param>
+/// <returns>Shift_JIS文字列</returns>
+/// <remarks>
+/// https://nekko1119.hatenablog.com/entry/2017/01/02/054629 から引用
+/// </remarks>
 std::string wide_to_multi_capi(std::wstring const& src) {
   std::size_t converted{};
   std::vector<char> dest(src.size() * sizeof(wchar_t) + 1, '\0');
@@ -81,14 +106,15 @@ std::string wide_to_multi_capi(std::wstring const& src) {
   return std::string(dest.begin(), dest.end());
 }
 
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント:
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] >
-//   [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+/// <summary>
+/// ワイド文字列をUTF8に変換します。
+/// </summary>
+/// <param name="src">ワイド文字列</param>
+/// <returns>UTF8文字列</returns>
+/// <remarks>
+/// https://nekko1119.hatenablog.com/entry/2017/01/02/054629 から引用
+/// </remarks>
+std::string wide_to_utf8_cppapi(std::wstring const& src) {
+  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+  return converter.to_bytes(src);
+}
--- example/cpp/windows/simple_tts/simple_tts.h
+++ example/cpp/windows/simple_tts/simple_tts.h
@@ -5,3 +5,4 @@ std::string GetOpenJTalkDict();
 std::wstring GetExePath();
 std::wstring GetExeDirectory();
 std::string wide_to_multi_capi(std::wstring const& src);
+std::string wide_to_utf8_cppapi(std::wstring const& src);
