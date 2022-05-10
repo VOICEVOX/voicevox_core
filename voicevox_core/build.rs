65cd37bd455f9653f678a823baba57f6b656ce6f
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-05-10 22:17:58 +0900

    従来どおりの出力をするように修正
    - バイナリ名を従来通りcoreに変更
    - ヘッダを従来どおりcore.hに変更
    - workaroundでworkspaceのルートディレクトリを取得しその配下のtargetにヘッダを出力するようにした

--- voicevox_core/build.rs
+++ voicevox_core/build.rs
@@ -6,13 +6,7 @@ use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    let package_name = env::var("CARGO_PKG_NAME").unwrap();
-    let output_file = target_dir()
-        .join(format!("{}.h", package_name))
-        .display()
-        .to_string();
-
+    let output_file = target_dir().join("core.h").display().to_string();
     let config = Config {
         language: Language::C,
         sys_includes: vec!["stdint.h".into()],
@@ -41,9 +35,5 @@ __declspec(dllimport)
         .write_to_file(&output_file);
 }
 fn target_dir() -> PathBuf {
-    if let Ok(target) = env::var("CARGO_TARGET_DIR") {
-        PathBuf::from(target)
-    } else {
-        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
-    }
+    PathBuf::from(env::var("CARGO_WORKSPACE_DIR").unwrap()).join("target")
 }
