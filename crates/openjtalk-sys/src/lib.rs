aa1268f63a15e7df302ddf2f1a3cd1821f4c70b9
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-06-26 21:45:38 +0900

    includeファイルを出力するようにした

--- crates/openjtalk-sys/src/lib.rs
+++ crates/openjtalk-sys/src/lib.rs
@@ -1,11 +1,12 @@
 extern crate link_cplusplus;
 #[allow(clippy::all)]
 #[allow(warnings)]
+#[cfg(not(feature = "generate-bindings"))]
 mod bindings {
-    #[cfg(not(feature = "generate_bindings"))]
     include!(concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/src/generated/bindings.rs"
     ));
 }
+#[cfg(not(feature = "generate-bindings"))]
 pub use bindings::*;
