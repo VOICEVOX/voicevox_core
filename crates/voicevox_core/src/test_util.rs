1d979252bd07838c9c32295e92f2aeddca5e1924
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-07-15 01:29:56 +0900

    まだふようのためcreate_user_dict周りの実装とデータを削除

--- crates/voicevox_core/src/test_util.rs
+++ crates/voicevox_core/src/test_util.rs
@@ -1,8 +1,4 @@
-use std::{
-    collections::hash_map::DefaultHasher,
-    hash::{Hash, Hasher},
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
 use async_std::{fs, io::ReadExt, sync::Mutex};
 use flate2::read::GzDecoder;
@@ -11,43 +7,6 @@ use tar::Archive;
 
 const DIC_DIR_NAME: &str = "open_jtalk_dic_utf_8-1.11";
 static OPEN_JTALK_DIC_DIR: Lazy<Mutex<Option<PathBuf>>> = Lazy::new(|| Mutex::new(None));
-
-#[allow(dead_code)]
-pub async fn create_user_dict_if_not_exists_for_test(path: impl AsRef<Path>) -> PathBuf {
-    let path = path.as_ref();
-    let mut hasher = DefaultHasher::new();
-    path.to_str().unwrap().hash(&mut hasher);
-    let out_path = PathBuf::from(env!("OUT_DIR"))
-        .join("testdata")
-        .join(hasher.finish().to_string());
-    if !out_path.exists() {
-        create_user_dict(path, &out_path).await;
-    }
-    out_path
-}
-
-async fn create_user_dict(path: impl AsRef<Path>, out_path: impl AsRef<Path>) {
-    let open_jtalk_dic_dir = download_open_jtalk_dict_if_no_exists().await;
-    let out_path = out_path.as_ref();
-    if !out_path.exists() {
-        fs::create_dir_all(out_path.parent().unwrap())
-            .await
-            .unwrap();
-
-        open_jtalk::mecab_dict_index(&[
-            "mecab-dict-index",
-            "-d",
-            open_jtalk_dic_dir.to_str().unwrap(),
-            "-u",
-            out_path.to_str().unwrap(),
-            "-f",
-            "utf-8",
-            "-t",
-            "utf-8",
-            path.as_ref().to_str().unwrap(),
-        ]);
-    }
-}
 
 pub async fn download_open_jtalk_dict_if_no_exists() -> PathBuf {
     let mut open_jtalk_dic_dir = OPEN_JTALK_DIC_DIR.lock().await;
