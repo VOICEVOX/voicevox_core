54170c1cafc4f0b00a45c9abb02c6011559a7f89
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-07-14 23:44:19 +0900

    extract_fullcontextのテストを追加

--- crates/voicevox_core/src/engine/open_jtalk.rs
+++ crates/voicevox_core/src/engine/open_jtalk.rs
@@ -94,3 +94,113 @@ impl OpenJtalk {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    use crate::*;
+
+    fn testdata_hello_hiho() -> Vec<String> {
+        // こんにちは、ヒホです。の期待値
+        vec![
+            // sil (無音)
+            String::from(
+                "xx^xx-sil+k=o/A:xx+xx+xx/B:xx-xx_xx/C:xx_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx",
+            ) + "/F:xx_xx#xx_x@xx_xx|xx_xx/G:5_5%0_xx_xx/H:xx_xx/I:xx-xx"
+                + "@xx+xx&xx-xx|xx+xx/J:1_5/K:2+2-9",
+            // k
+            String::from("xx^sil-k+o=N/A:-4+1+5/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // o
+            String::from("sil^k-o+N=n/A:-4+1+5/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // N (ん)
+            String::from("k^o-N+n=i/A:-3+2+4/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // n
+            String::from("o^N-n+i=ch/A:-2+3+3/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // i
+            String::from("N^n-i+ch=i/A:-2+3+3/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // ch
+            String::from("n^i-ch+i=w/A:-1+4+2/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // i
+            String::from("i^ch-i+w=a/A:-1+4+2/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // w
+            String::from("ch^i-w+a=pau/A:0+5+1/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // a
+            String::from("i^w-a+pau=h/A:0+5+1/B:xx-xx_xx/C:09_xx+xx/D:09+xx_xx/E:xx_xx!xx_xx-xx")
+                + "/F:5_5#0_xx@1_1|1_5/G:4_1%0_xx_0/H:xx_xx/I:1-5"
+                + "@1+2&1-2|1+9/J:1_4/K:2+2-9",
+            // pau (読点)
+            String::from("w^a-pau+h=i/A:xx+xx+xx/B:09-xx_xx/C:xx_xx+xx/D:09+xx_xx/E:5_5!0_xx-xx")
+                + "/F:xx_xx#xx_xx@xx_xx|xx_xx/G:4_1%0_xx_xx/H:1_5/I:xx-xx"
+                + "@xx+xx&xx-xx|xx+xx/J:1_4/K:2+2-9",
+            // h
+            String::from("a^pau-h+i=h/A:0+1+4/B:09-xx_xx/C:09_xx+xx/D:22+xx_xx/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // i
+            String::from("pau^h-i+h=o/A:0+1+4/B:09-xx_xx/C:09_xx+xx/D:22+xx_xx/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // h
+            String::from("h^i-h+o=d/A:1+2+3/B:09-xx_xx/C:22_xx+xx/D:10+7_2/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // o
+            String::from("i^h-o+d=e/A:1+2+3/B:09-xx_xx/C:22_xx+xx/D:10+7_2/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // d
+            String::from("h^o-d+e=s/A:2+3+2/B:22-xx_xx/C:10_7+2/D:xx+xx_xx/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // e
+            String::from("o^d-e+s=U/A:2+3+2/B:22-xx_xx/C:10_7+2/D:xx+xx_xx/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // s
+            String::from("d^e-s+U=sil/A:3+4+1/B:22-xx_xx/C:10_7+2/D:xx+xx_xx/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // U (無声母音)
+            String::from("e^s-U+sil=xx/A:3+4+1/B:22-xx_xx/C:10_7+2/D:xx+xx_xx/E:5_5!0_xx-0")
+                + "/F:4_1#0_xx@1_1|1_4/G:xx_xx%xx_xx_xx/H:1_5/I:1-4"
+                + "@2+1&2-1|6+4/J:xx_xx/K:2+2-9",
+            // sil (無音)
+            String::from("s^U-sil+xx=xx/A:xx+xx+xx/B:10-7_2/C:xx_xx+xx/D:xx+xx_xx/E:4_1!0_xx-xx")
+                + "/F:xx_xx#xx_xx@xx_xx|xx_xx/G:xx_xx%xx_xx_xx/H:1_4/I:xx-xx"
+                + "@xx+xx&xx-xx|xx+xx/J:xx_xx/K:2+2-9",
+        ]
+    }
+
+    #[rstest]
+    #[case("",Err(OpenJtalkError::ExtractFullContext{text:"".into(),source:None}))]
+    #[case("こんにちは、ヒホです。", Ok(testdata_hello_hiho()))]
+    #[async_std::test]
+    async fn extract_fullcontext_works(
+        #[case] text: &str,
+        #[case] expected: super::Result<Vec<String>>,
+    ) {
+        let open_jtalk_dic_dir = download_open_jtalk_dict_if_no_exists().await;
+        let mut open_jtalk = OpenJtalk::initialize();
+        open_jtalk.load(&open_jtalk_dic_dir).unwrap();
+        let result = open_jtalk.extract_fullcontext(text);
+        assert_eq!(expected, result);
+    }
+}
