c998466627914077e7463974a6841bfb9a6393df
PickledChair <ubatamamoon@gmail.com>
2022-08-20 22:25:55 +0900

    add rpath setting for macos dylib

--- crates/voicevox_core_c_api/build.rs
+++ crates/voicevox_core_c_api/build.rs
@@ -6,6 +6,8 @@ fn main() {
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 
     #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/");
+    #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-arg=-Wl,-install_name,@rpath/libcore.dylib");
 }
 
