8e4802727fe59a6e4dfde7b6d2e3924c184889b8
Gray Suitcase <41382894+PickledChair@users.noreply.github.com>
2022-08-21 07:34:11 +0900

    Update crates/voicevox_core_c_api/build.rs
    Co-authored-by: qwerty2501 <939468+qwerty2501@users.noreply.github.com>

--- crates/voicevox_core_c_api/build.rs
+++ crates/voicevox_core_c_api/build.rs
@@ -6,9 +6,10 @@ fn main() {
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 
     #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/");
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-install_name,@rpath/libcore.dylib");
+    {
+        println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/");
+        println!("cargo:rustc-link-arg=-Wl,-install_name,@rpath/libcore.dylib");
+    }
 }
 
 #[cfg(feature = "generate-c-header")]
