541de3a1e2613311eca6fc1cc30e0dbd144f082c
Ryo Yamashita <qryxip@gmail.com>
2022-09-19 03:40:38 +0900

    cbindgenの設定をcbindgen.tomlに移行 (#284)
    Migrate to cbindgen.toml

--- crates/voicevox_core_c_api/build.rs
+++ crates/voicevox_core_c_api/build.rs
@@ -14,51 +14,13 @@ fn main() {
 
 #[cfg(feature = "generate-c-header")]
 fn generate_c_header() {
-    use cbindgen::{Config, EnumConfig, FunctionConfig, Language, ParseConfig, RenameRule};
     use std::env;
     use std::path::PathBuf;
 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let output_file = target_dir().join("voicevox_core.h").display().to_string();
-    let config = Config {
-        parse: ParseConfig {
-            parse_deps: true,
-            include: Some(vec!["voicevox_core".to_string()]),
-            ..Default::default()
-        },
-        language: Language::C,
-        header: Some("/// @file".into()),
-        no_includes: true,
-        after_includes: Some(
-            r#"#ifdef __cplusplus
-#include <cstdint>
-#else // __cplusplus
-#include <stdbool.h>
-#include <stdint.h>
-#endif // __cplusplus"#
-                .into(),
-        ),
-        cpp_compat: true,
-        include_guard: Some("VOICEVOX_CORE_INCLUDE_GUARD".into()),
-        function: FunctionConfig {
-            prefix: Some(
-                r#"#ifdef _WIN32
-__declspec(dllimport)
-#endif
-"#
-                .into(),
-            ),
-            ..Default::default()
-        },
-        documentation_style: cbindgen::DocumentationStyle::Doxy,
-        enumeration: EnumConfig {
-            rename_variants: RenameRule::ScreamingSnakeCase,
-            ..Default::default()
-        },
-        ..Default::default()
-    };
 
-    cbindgen::generate_with_config(&crate_dir, config)
+    cbindgen::generate(&crate_dir)
         .unwrap()
         .write_to_file(&output_file);
 
