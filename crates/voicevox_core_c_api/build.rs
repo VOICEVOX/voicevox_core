de8bc5d71b6891af143d7e44a4fc27e46d8342aa
Ryo Yamashita <qryxip@gmail.com>
2022-09-19 23:34:36 +0900

    Remove `voicevox_core_c_api/generate-c-header`

--- crates/voicevox_core_c_api/build.rs
+++ crates/voicevox_core_c_api/build.rs
@@ -1,7 +1,4 @@
 fn main() {
-    #[cfg(feature = "generate-c-header")]
-    generate_c_header();
-
     #[cfg(target_os = "linux")]
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 
@@ -9,22 +6,5 @@ fn main() {
     {
         println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/");
         println!("cargo:rustc-link-arg=-Wl,-install_name,@rpath/libvoicevox_core.dylib");
-    }
-}
-
-#[cfg(feature = "generate-c-header")]
-fn generate_c_header() {
-    use std::env;
-    use std::path::PathBuf;
-
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let output_file = target_dir().join("voicevox_core.h").display().to_string();
-
-    cbindgen::generate(&crate_dir)
-        .unwrap()
-        .write_to_file(&output_file);
-
-    fn target_dir() -> PathBuf {
-        PathBuf::from(env::var("CARGO_WORKSPACE_DIR").unwrap()).join("target")
     }
 }
