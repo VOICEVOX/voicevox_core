28cbee8f1ecee4dd8dcb8fd572d98038c97ef84f
Hiroshiba <hihokaruta@gmail.com>
2023-01-04 02:14:36 +0900

    モデルを２つにする

--- crates/voicevox_core/src/include_models.rs
+++ crates/voicevox_core/src/include_models.rs
@@ -2,15 +2,29 @@
     Model{
         predict_duration_model: include_bytes!(concat!(
         env!("CARGO_WORKSPACE_DIR"),
-        "/model/predict_duration.onnx"
+        "/model/predict_duration-0.onnx"
     )),
         predict_intonation_model: include_bytes!(concat!(
         env!("CARGO_WORKSPACE_DIR"),
-        "/model/predict_intonation.onnx"
+        "/model/predict_intonation-0.onnx"
     )),
         decode_model: include_bytes!(concat!(
         env!("CARGO_WORKSPACE_DIR"), 
-        "/model/decode.onnx"
+        "/model/decode-0.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration-1.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation-1.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode-1.onnx"
     )),
     },
 ]
