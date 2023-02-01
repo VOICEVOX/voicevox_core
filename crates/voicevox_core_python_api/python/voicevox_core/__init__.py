2f7a5120dffd99457099da2eba5b05fa2326d61f
Ryo Yamashita <qryxip@gmail.com>
2023-02-02 02:21:51 +0900

    __init__.pyに`__all__`を追加

--- crates/voicevox_core_python_api/python/voicevox_core/__init__.py
+++ crates/voicevox_core_python_api/python/voicevox_core/__init__.py
@@ -9,3 +9,16 @@ from ._models import (  # noqa: F401
     SupportedDevices,
 )
 from ._rust import METAS, SUPPORTED_DEVICES, VoicevoxCore  # noqa: F401
+
+
+__all__ = [
+    "AccelerationMode",
+    "AccentPhrase",
+    "AudioQuery",
+    "METAS",
+    "Meta",
+    "Mora",
+    "SUPPORTED_DEVICES",
+    "SupportedDevices",
+    "VoicevoxCore",
+]
