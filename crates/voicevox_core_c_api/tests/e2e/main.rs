454a1940c0fe9ba89794cbecc21633239f142a85
Ryo Yamashita <qryxip@gmail.com>
2023-02-11 02:52:47 +0900

    Revert "`cargo build -p voicevox_core_c_api --lib`を単体で実行"
    This reverts commit 6473c8d0fa989ddda921d610a2b883809835a533.

--- crates/voicevox_core_c_api/tests/e2e/main.rs
+++ crates/voicevox_core_c_api/tests/e2e/main.rs
@@ -32,17 +32,7 @@ fn main() -> anyhow::Result<()> {
             env!("CARGO_PKG_NAME"),
             "--lib",
         )
-        .run()
-        .map_err(|err| {
-            let mut err = anyhow::Error::from(err);
-            if cfg!(windows) {
-                err = err.context(format!(
-                    "Windowsの場合、先に`cargo build -p {} --lib`を単体で実行して下さい",
-                    env!("CARGO_PKG_NAME"),
-                ));
-            }
-            err
-        })?;
+        .run()?;
     }
 
     let tests = Test::value_variants()
