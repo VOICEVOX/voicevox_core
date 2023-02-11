6fd8d378c2b6b40561a96303ef0c5c71005e0260
Ryo Yamashita <qryxip@gmail.com>
2023-02-11 15:02:46 +0900

    `ORT_OUT_DIR=`についてコメント

--- crates/voicevox_core_c_api/tests/e2e/main.rs
+++ crates/voicevox_core_c_api/tests/e2e/main.rs
@@ -31,6 +31,11 @@ fn main() -> anyhow::Result<()> {
     // そのためスキップするのはCLIオプションに`--ignored`か`--include-ignored`が無いときのみ
     if args.ignored || args.include_ignored {
         cmd!(env!("CARGO"), "build", "--lib")
+            // 他の単体テストが動いているときにonnxruntime-sysの初回ビルドを行うと、Windows環境だと
+            // `$ORT_OUT_DIR`のハックが問題を起こす。そのためこのハック自体を無効化する
+            //
+            // featuresの差分を出さないように`cargo build`することができればonnxruntime-sysの
+            // ビルド自体がされないのだが、このバイナリから`cargo build`の状況を知るのは無理に近い
             .env("ORT_OUT_DIR", "")
             .run()?;
     }
