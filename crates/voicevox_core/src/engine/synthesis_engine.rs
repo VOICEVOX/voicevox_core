3f66e61fc81573cea6990a9a0b72c284ce8b1b69
Ryo Yamashita <qryxip@gmail.com>
2023-05-21 17:30:47 +0900

    無駄な`async {}`を外す

--- crates/voicevox_core/src/engine/synthesis_engine.rs
+++ crates/voicevox_core/src/engine/synthesis_engine.rs
@@ -432,55 +432,52 @@ impl SynthesisEngine {
         style_id: StyleId,
         enable_interrogative_upspeak: bool,
     ) -> Result<Vec<u8>> {
-        async {
-            let wave = self
-                .synthesis(query, style_id, enable_interrogative_upspeak)
-                .await?;
-            let volume_scale = *query.volume_scale();
-            let output_stereo = *query.output_stereo();
-            let output_sampling_rate = *query.output_sampling_rate();
+        let wave = self
+            .synthesis(query, style_id, enable_interrogative_upspeak)
+            .await?;
+        let volume_scale = *query.volume_scale();
+        let output_stereo = *query.output_stereo();
+        let output_sampling_rate = *query.output_sampling_rate();
 
-            // TODO: 44.1kHzなどの対応
+        // TODO: 44.1kHzなどの対応
 
-            let num_channels: u16 = if output_stereo { 2 } else { 1 };
-            let bit_depth: u16 = 16;
-            let repeat_count: u32 =
-                (output_sampling_rate / Self::DEFAULT_SAMPLING_RATE) * num_channels as u32;
-            let block_size: u16 = bit_depth * num_channels / 8;
+        let num_channels: u16 = if output_stereo { 2 } else { 1 };
+        let bit_depth: u16 = 16;
+        let repeat_count: u32 =
+            (output_sampling_rate / Self::DEFAULT_SAMPLING_RATE) * num_channels as u32;
+        let block_size: u16 = bit_depth * num_channels / 8;
 
-            let bytes_size = wave.len() as u32 * repeat_count * 2;
-            let wave_size = bytes_size + 44;
+        let bytes_size = wave.len() as u32 * repeat_count * 2;
+        let wave_size = bytes_size + 44;
 
-            let buf: Vec<u8> = Vec::with_capacity(wave_size as usize);
-            let mut cur = Cursor::new(buf);
+        let buf: Vec<u8> = Vec::with_capacity(wave_size as usize);
+        let mut cur = Cursor::new(buf);
 
-            cur.write_all("RIFF".as_bytes()).unwrap();
-            cur.write_all(&(wave_size - 8).to_le_bytes()).unwrap();
-            cur.write_all("WAVEfmt ".as_bytes()).unwrap();
-            cur.write_all(&16_u32.to_le_bytes()).unwrap(); // fmt header length
-            cur.write_all(&1_u16.to_le_bytes()).unwrap(); //linear PCM
-            cur.write_all(&num_channels.to_le_bytes()).unwrap();
-            cur.write_all(&output_sampling_rate.to_le_bytes()).unwrap();
+        cur.write_all("RIFF".as_bytes()).unwrap();
+        cur.write_all(&(wave_size - 8).to_le_bytes()).unwrap();
+        cur.write_all("WAVEfmt ".as_bytes()).unwrap();
+        cur.write_all(&16_u32.to_le_bytes()).unwrap(); // fmt header length
+        cur.write_all(&1_u16.to_le_bytes()).unwrap(); //linear PCM
+        cur.write_all(&num_channels.to_le_bytes()).unwrap();
+        cur.write_all(&output_sampling_rate.to_le_bytes()).unwrap();
 
-            let block_rate = output_sampling_rate * block_size as u32;
+        let block_rate = output_sampling_rate * block_size as u32;
 
-            cur.write_all(&block_rate.to_le_bytes()).unwrap();
-            cur.write_all(&block_size.to_le_bytes()).unwrap();
-            cur.write_all(&bit_depth.to_le_bytes()).unwrap();
-            cur.write_all("data".as_bytes()).unwrap();
-            cur.write_all(&bytes_size.to_le_bytes()).unwrap();
+        cur.write_all(&block_rate.to_le_bytes()).unwrap();
+        cur.write_all(&block_size.to_le_bytes()).unwrap();
+        cur.write_all(&bit_depth.to_le_bytes()).unwrap();
+        cur.write_all("data".as_bytes()).unwrap();
+        cur.write_all(&bytes_size.to_le_bytes()).unwrap();
 
-            for value in wave {
-                let v = (value * volume_scale).clamp(-1., 1.);
-                let data = (v * 0x7fff as f32) as i16;
-                for _ in 0..repeat_count {
-                    cur.write_all(&data.to_le_bytes()).unwrap();
-                }
+        for value in wave {
+            let v = (value * volume_scale).clamp(-1., 1.);
+            let data = (v * 0x7fff as f32) as i16;
+            for _ in 0..repeat_count {
+                cur.write_all(&data.to_le_bytes()).unwrap();
             }
-
-            Ok(cur.into_inner())
         }
-        .await
+
+        Ok(cur.into_inner())
     }
 
     pub fn is_openjtalk_dict_loaded(&self) -> bool {
