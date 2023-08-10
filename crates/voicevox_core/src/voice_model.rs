2be5936fd8be1f7ec1e898cf366e3be93331ee83
Yusuke Yamada <yamachu.dev@gmail.com>
2023-08-11 00:12:17 +0900

    Update crates/voicevox_core/src/voice_model.rs
    Co-authored-by: Ryo Yamashita <qryxip@gmail.com>

--- crates/voicevox_core/src/voice_model.rs
+++ crates/voicevox_core/src/voice_model.rs
@@ -113,12 +113,16 @@ impl VoiceModel {
         let mut vvm_paths = Vec::new();
         for entry in root_dir
             .read_dir()
+            .and_then(|entries| entries.collect::<std::result::Result<Vec<_>, _>>())
+            .map(|entries| {
+                entries
+                    .into_iter()
+                    .filter(|entry| entry.path().extension().map_or(false, |ext| ext == "vvm"))
+            })
             .map_err(|e| Error::LoadModel {
                 path: root_dir.clone(),
                 source: e.into(),
             })?
-            .filter_map(|v| v.ok())
-            .filter(|entry| entry.path().extension().map_or(false, |ext| ext == "vvm"))
         {
             vvm_paths.push(Self::from_path(entry.path()));
         }
