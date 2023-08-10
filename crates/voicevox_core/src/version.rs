6e5577a7e61ce5fdecef96a021dfb3b269db7480
shigobu <shigobu@shigobu.com>
2023-08-10 22:07:16 +0900

    version.rsを修正

--- crates/voicevox_core/src/version.rs
+++ crates/voicevox_core/src/version.rs
@@ -1,6 +1,8 @@
-pub const fn get_version() -> &'static str {
-    env!("CARGO_PKG_VERSION")
-}
+/// 本クレートの`package.version`。
+///
+/// C APIやPython API側からこの値が使われるべきではない。
+/// 現在はまだRust APIを外部提供していないため、この定数はどこからも参照されていないはずである。
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg(test)]
 mod tests {
@@ -8,6 +10,6 @@ mod tests {
     use crate::*;
     #[rstest]
     fn get_version_works() {
-        assert_eq!("0.0.0", get_version());
+        assert_eq!("0.0.0", VERSION);
     }
 }
