47f5388b5191bf2c0ad2db02794a95452fd95946
Ryo Yamashita <qryxip@gmail.com>
2023-08-13 00:52:42 +0900

    `OpenJtalk`をfixtureに

--- crates/voicevox_core_python_api/python/test/test_pseudo_raii_for_synthesizer.py
+++ crates/voicevox_core_python_api/python/test/test_pseudo_raii_for_synthesizer.py
@@ -4,17 +4,13 @@ from voicevox_core import OpenJtalk, Synthesizer, VoicevoxError
 
 
 @pytest.mark.asyncio
-async def test_enter_returns_workable_self() -> None:
-    open_jtalk = OpenJtalk(conftest.open_jtalk_dic_dir)
-
+async def test_enter_returns_workable_self(open_jtalk: OpenJtalk) -> None:
     with await Synthesizer.new_with_initialize(open_jtalk) as synthesizer:
         synthesizer.metas
 
 
 @pytest.mark.asyncio
-async def test_closing_multiple_times_is_allowed() -> None:
-    open_jtalk = OpenJtalk(conftest.open_jtalk_dic_dir)
-
+async def test_closing_multiple_times_is_allowed(open_jtalk: OpenJtalk) -> None:
     with await Synthesizer.new_with_initialize(open_jtalk) as synthesizer:
         with synthesizer:
             pass
@@ -24,9 +20,7 @@ async def test_closing_multiple_times_is_allowed() -> None:
 
 
 @pytest.mark.asyncio
-async def test_access_after_close_denied() -> None:
-    open_jtalk = OpenJtalk(conftest.open_jtalk_dic_dir)
-
+async def test_access_after_close_denied(open_jtalk: OpenJtalk) -> None:
     synthesizer = await Synthesizer.new_with_initialize(open_jtalk)
     synthesizer.close()
     with pytest.raises(VoicevoxError, match="^The `Synthesizer` is closed$"):
@@ -34,10 +28,13 @@ async def test_access_after_close_denied() -> None:
 
 
 @pytest.mark.asyncio
-async def test_access_after_exit_denied() -> None:
-    open_jtalk = OpenJtalk(conftest.open_jtalk_dic_dir)
-
+async def test_access_after_exit_denied(open_jtalk: OpenJtalk) -> None:
     with await Synthesizer.new_with_initialize(open_jtalk) as synthesizer:
         pass
     with pytest.raises(VoicevoxError, match="^The `Synthesizer` is closed$"):
         synthesizer.metas
+
+
+@pytest.fixture(scope="module")
+def open_jtalk() -> OpenJtalk:
+    return OpenJtalk(conftest.open_jtalk_dic_dir)
