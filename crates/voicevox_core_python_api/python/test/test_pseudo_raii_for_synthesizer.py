244acb6915480b93f9aad4571cdf241fac97174f
Ryo Yamashita <qryxip@gmail.com>
2023-08-13 00:38:08 +0900

    test_pseudo_raii_for_synthesizer.pyを更新

--- crates/voicevox_core_python_api/python/test/test_pseudo_raii_for_synthesizer.py
+++ crates/voicevox_core_python_api/python/test/test_pseudo_raii_for_synthesizer.py
@@ -4,11 +4,23 @@ from voicevox_core import OpenJtalk, Synthesizer, VoicevoxError
 
 
 @pytest.mark.asyncio
-async def test_enter() -> None:
+async def test_enter_returns_workable_self() -> None:
     open_jtalk = OpenJtalk(conftest.open_jtalk_dic_dir)
 
     with await Synthesizer.new_with_initialize(open_jtalk) as synthesizer:
         synthesizer.metas
+
+
+@pytest.mark.asyncio
+async def test_closing_multiple_times_is_allowed() -> None:
+    open_jtalk = OpenJtalk(conftest.open_jtalk_dic_dir)
+
+    with await Synthesizer.new_with_initialize(open_jtalk) as synthesizer:
+        with synthesizer:
+            pass
+
+    synthesizer.close()
+    synthesizer.close()
 
 
 @pytest.mark.asyncio
