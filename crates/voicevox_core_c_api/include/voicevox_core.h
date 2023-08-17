60cad17906df426488869c99ec60c06ec3b0267f
Ryo Yamashita <qryxip@gmail.com>
2023-08-18 07:27:11 +0900

    `cargo xtask update-c-header`

--- crates/voicevox_core_c_api/include/voicevox_core.h
+++ crates/voicevox_core_c_api/include/voicevox_core.h
@@ -149,11 +149,11 @@ enum VoicevoxResultCode
   /**
    * すでに読み込まれているスタイルを読み込もうとした
    */
-  VOICEVOX_RESULT_STYLE_ALREADY_LOADED_ERROR = 2,
+  VOICEVOX_RESULT_STYLE_ALREADY_LOADED_ERROR = 26,
   /**
    * 無効なモデルデータ
    */
-  VOICEVOX_RESULT_INVALID_MODEL_DATA_ERROR = 5,
+  VOICEVOX_RESULT_INVALID_MODEL_DATA_ERROR = 27,
   /**
    * Modelが読み込まれていない
    */
