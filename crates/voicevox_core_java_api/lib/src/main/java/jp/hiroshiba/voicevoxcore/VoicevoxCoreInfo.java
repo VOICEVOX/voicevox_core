d01d996a4406f0bd676b32f216a2020e59bba16c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-11-03 10:41:58 +0900

    Fix: ドキュメントのリンク周りを修正

--- crates/voicevox_core_java_api/lib/src/main/java/jp/hiroshiba/voicevoxcore/VoicevoxCoreInfo.java
+++ crates/voicevox_core_java_api/lib/src/main/java/jp/hiroshiba/voicevoxcore/VoicevoxCoreInfo.java
@@ -11,6 +11,8 @@ import com.google.gson.annotations.SerializedName;
 public class VoicevoxCoreInfo extends Dll {
   /**
    * ライブラリのバージョン。
+   *
+   * @return ライブラリのバージョン。
    */
   @Nonnull
   public static String getVersion() {
@@ -20,7 +22,7 @@ public class VoicevoxCoreInfo extends Dll {
   /**
    * このライブラリで利用可能なデバイスの情報を取得する。
    *
-   * @see SupportedDevices
+   * @return {@link SupportedDevices}。
    */
   @Nonnull
   public static SupportedDevices getSupportedDevices() {
@@ -39,9 +41,9 @@ public class VoicevoxCoreInfo extends Dll {
    * このライブラリで利用可能なデバイスの情報。
    *
    * あくまで本ライブラリが対応しているデバイスの情報であることに注意。GPUが使える環境ではなかったとしても
-   * <a href="#cuda">cuda</a> や <a href="#dml">dml</a> は <code>true</code> を示しうる。
+   * {@link #cuda} や {@link #dml} は {@code true} を示しうる。
    */
-  static class SupportedDevices {
+  public static class SupportedDevices {
     /**
      * CPUが利用可能。
      *
@@ -56,7 +58,8 @@ public class VoicevoxCoreInfo extends Dll {
      * CUDAが利用可能。
      *
      * ONNX Runtimeの <a href=
-     * "https://onnxruntime.ai/docs/execution-providers/CUDA-ExecutionProvider.html">CUDAExecutionProvider</a>に対応する。
+     * "https://onnxruntime.ai/docs/execution-providers/CUDA-ExecutionProvider.html"
+     * target="_blank">CUDAExecutionProvider</a>に対応する。
      * 必要な環境についてはそちらを参照。
      */
     @SerializedName("cuda")
@@ -68,7 +71,8 @@ public class VoicevoxCoreInfo extends Dll {
      * DirectMLが利用可能。
      *
      * ONNX Runtimeの <a href=
-     * "https://onnxruntime.ai/docs/execution-providers/DirectML-ExecutionProvider.html">DmlExecutionProvider</a>に対応する。
+     * "https://onnxruntime.ai/docs/execution-providers/DirectML-ExecutionProvider.html"
+     * target="_blank">DmlExecutionProvider</a>に対応する。
      * 必要な環境についてはそちらを参照。
      */
     @SerializedName("dml")
