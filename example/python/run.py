d7040a7dd5c9afd09665cc88a663f6e35ea047fd
Hiroshiba <hihokaruta@gmail.com>
2023-11-10 22:26:28 +0900

    pythonサンプルが実行できなかったので修正

--- example/python/run.py
+++ example/python/run.py
@@ -36,7 +36,7 @@ async def main() -> None:
     logger.debug("%s", f"{voicevox_core.supported_devices()=}")
 
     logger.info("%s", f"Initializing ({acceleration_mode=}, {open_jtalk_dict_dir=})")
-    synthesizer = Synthesizer(
+    synthesizer = await Synthesizer.new_with_initialize(
         OpenJtalk(open_jtalk_dict_dir), acceleration_mode=acceleration_mode
     )
 
