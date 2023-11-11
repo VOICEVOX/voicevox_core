120106b8d38d4430d7b64b3720fb78ec1a7d09e8
Ryo Yamashita <qryxip@gmail.com>
2023-11-11 17:48:28 +0900

    voicevox_core_macrosを作り、"signatures"の実装をマクロ化

--- crates/voicevox_core/src/infer/signatures.rs
+++ crates/voicevox_core/src/infer/signatures.rs
@@ -1,11 +1,8 @@
-use anyhow::ensure;
 use enum_map::Enum;
+use macros::{InferenceInputSignature, TryFromVecAnyTensor};
 use ndarray::{Array0, Array1, Array2};
 
-use super::{
-    AnyTensor, InferenceGroup, InferenceInputSignature, InferenceRuntime, InferenceSignature,
-    RunContextExt as _,
-};
+use super::{AnyTensor, InferenceGroup, InferenceSignature};
 
 pub(crate) enum InferenceGroupImpl {}
 
@@ -29,44 +26,16 @@ impl InferenceSignature for PredictDuration {
     const INFERENCE: InferencelKindImpl = InferencelKindImpl::PredictDuration;
 }
 
+#[derive(InferenceInputSignature)]
+#[input_signature(Signature = PredictDuration)]
 pub(crate) struct PredictDurationInput {
     pub(crate) phoneme: Array1<i64>,
     pub(crate) speaker_id: Array1<i64>,
 }
 
-impl InferenceInputSignature for PredictDurationInput {
-    type Signature = PredictDuration;
-
-    fn make_run_context<R: InferenceRuntime>(self, sess: &mut R::Session) -> R::RunContext<'_> {
-        R::RunContext::from(sess)
-            .with_input(self.phoneme)
-            .with_input(self.speaker_id)
-    }
-}
-
+#[derive(TryFromVecAnyTensor)]
 pub(crate) struct PredictDurationOutput {
     pub(crate) phoneme_length: Array1<f32>,
-}
-
-impl TryFrom<Vec<AnyTensor>> for PredictDurationOutput {
-    type Error = anyhow::Error;
-
-    fn try_from(tensors: Vec<AnyTensor>) -> Result<Self, Self::Error> {
-        ensure!(
-            tensors.len() == 1,
-            "expected 1 tensor(s), got {}",
-            tensors.len(),
-        );
-
-        let mut tensors = tensors.into_iter();
-        let this = Self {
-            phoneme_length: tensors
-                .next()
-                .expect("the length should have been checked")
-                .try_into()?,
-        };
-        Ok(this)
-    }
 }
 
 pub(crate) enum PredictIntonation {}
@@ -78,6 +47,8 @@ impl InferenceSignature for PredictIntonation {
     const INFERENCE: InferencelKindImpl = InferencelKindImpl::PredictIntonation;
 }
 
+#[derive(InferenceInputSignature)]
+#[input_signature(Signature = PredictIntonation)]
 pub(crate) struct PredictIntonationInput {
     pub(crate) length: Array0<i64>,
     pub(crate) vowel_phoneme: Array1<i64>,
@@ -89,45 +60,9 @@ pub(crate) struct PredictIntonationInput {
     pub(crate) speaker_id: Array1<i64>,
 }
 
-impl InferenceInputSignature for PredictIntonationInput {
-    type Signature = PredictIntonation;
-
-    fn make_run_context<R: InferenceRuntime>(self, sess: &mut R::Session) -> R::RunContext<'_> {
-        R::RunContext::from(sess)
-            .with_input(self.length)
-            .with_input(self.vowel_phoneme)
-            .with_input(self.consonant_phoneme)
-            .with_input(self.start_accent)
-            .with_input(self.end_accent)
-            .with_input(self.start_accent_phrase)
-            .with_input(self.end_accent_phrase)
-            .with_input(self.speaker_id)
-    }
-}
-
+#[derive(TryFromVecAnyTensor)]
 pub(crate) struct PredictIntonationOutput {
     pub(crate) f0_list: Array1<f32>,
-}
-
-impl TryFrom<Vec<AnyTensor>> for PredictIntonationOutput {
-    type Error = anyhow::Error;
-
-    fn try_from(tensors: Vec<AnyTensor>) -> Result<Self, Self::Error> {
-        ensure!(
-            tensors.len() == 1,
-            "expected 1 tensor(s), got {}",
-            tensors.len(),
-        );
-
-        let mut tensors = tensors.into_iter();
-        let this = Self {
-            f0_list: tensors
-                .next()
-                .expect("the length should have been checked")
-                .try_into()?,
-        };
-        Ok(this)
-    }
 }
 
 pub(crate) enum Decode {}
@@ -139,44 +74,15 @@ impl InferenceSignature for Decode {
     const INFERENCE: InferencelKindImpl = InferencelKindImpl::Decode;
 }
 
+#[derive(InferenceInputSignature)]
+#[input_signature(Signature = Decode)]
 pub(crate) struct DecodeInput {
     pub(crate) f0: Array2<f32>,
     pub(crate) phoneme: Array2<f32>,
     pub(crate) speaker_id: Array1<i64>,
 }
 
-impl InferenceInputSignature for DecodeInput {
-    type Signature = Decode;
-
-    fn make_run_context<R: InferenceRuntime>(self, sess: &mut R::Session) -> R::RunContext<'_> {
-        R::RunContext::from(sess)
-            .with_input(self.f0)
-            .with_input(self.phoneme)
-            .with_input(self.speaker_id)
-    }
-}
-
+#[derive(TryFromVecAnyTensor)]
 pub(crate) struct DecodeOutput {
     pub(crate) wave: Array1<f32>,
-}
-
-impl TryFrom<Vec<AnyTensor>> for DecodeOutput {
-    type Error = anyhow::Error;
-
-    fn try_from(tensors: Vec<AnyTensor>) -> Result<Self, Self::Error> {
-        ensure!(
-            tensors.len() == 1,
-            "expected 1 tensor(s), got {}",
-            tensors.len(),
-        );
-
-        let mut tensors = tensors.into_iter();
-        let this = Self {
-            wave: tensors
-                .next()
-                .expect("the length should have been checked")
-                .try_into()?,
-        };
-        Ok(this)
-    }
 }
---
+++ crates/voicevox_core_macros/src/lib.rs
@@ -0,0 +1,150 @@
+#![warn(rust_2018_idioms)]
+
+use quote::quote;
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+    spanned::Spanned as _,
+    Data, DataEnum, DataStruct, DataUnion, DeriveInput, Field, Fields, Token,
+};
+
+#[proc_macro_derive(InferenceInputSignature, attributes(input_signature))]
+pub fn derive_inference_input_signature(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    return derive_inference_input_signature(&parse_macro_input!(input))
+        .unwrap_or_else(|e| e.to_compile_error())
+        .into();
+
+    fn derive_inference_input_signature(
+        input: &DeriveInput,
+    ) -> syn::Result<proc_macro2::TokenStream> {
+        let DeriveInput {
+            attrs,
+            ident,
+            generics,
+            data,
+            ..
+        } = input;
+
+        let AssocTypeSignature(signature) = attrs
+            .iter()
+            .find(|a| a.path().is_ident("input_signature"))
+            .ok_or_else(|| {
+                syn::Error::new(
+                    proc_macro2::Span::call_site(),
+                    "missing `#[input_signature(…)]`",
+                )
+            })?
+            .parse_args()?;
+
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+        let field_names = struct_field_names(data)?;
+
+        Ok(quote! {
+            impl #impl_generics crate::infer::InferenceInputSignature for #ident #ty_generics
+            #where_clause
+            {
+                type Signature = #signature;
+
+                fn make_run_context<R: crate::infer::InferenceRuntime>(
+                    self,
+                    sess: &mut R::Session,
+                ) -> R::RunContext<'_> {
+                    let mut ctx = <R::RunContext<'_> as ::std::convert::From<_>>::from(sess);
+                    #(
+                        R::push_input(self.#field_names, &mut ctx);
+                    )*
+                    ctx
+                }
+            }
+        })
+    }
+
+    struct AssocTypeSignature(syn::Ident);
+
+    impl Parse for AssocTypeSignature {
+        fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
+            let key = input.parse::<syn::Ident>()?;
+            if key != "Signature" {
+                return Err(syn::Error::new(key.span(), "expected `Signature`"));
+            }
+            input.parse::<Token![=]>()?;
+            let value = input.parse::<syn::Ident>()?;
+            Ok(Self(value))
+        }
+    }
+}
+
+#[proc_macro_derive(TryFromVecAnyTensor)]
+pub fn derive_try_from_vec_any_tensor(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    return derive_try_from_vec_any_tensor(&parse_macro_input!(input))
+        .unwrap_or_else(|e| e.to_compile_error())
+        .into();
+
+    fn derive_try_from_vec_any_tensor(
+        input: &DeriveInput,
+    ) -> syn::Result<proc_macro2::TokenStream> {
+        let DeriveInput {
+            ident,
+            generics,
+            data,
+            ..
+        } = input;
+
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+        let field_names = struct_field_names(data)?;
+        let num_fields = field_names.len();
+
+        Ok(quote! {
+            impl #impl_generics
+                ::std::convert::TryFrom<::std::vec::Vec<crate::infer::AnyTensor>>
+                for #ident #ty_generics
+            #where_clause
+            {
+                type Error = ::anyhow::Error;
+
+                fn try_from(tensors: ::std::vec::Vec<AnyTensor>) -> ::std::result::Result<Self, Self::Error> {
+                    ::anyhow::ensure!(
+                        tensors.len() == #num_fields,
+                        "expected {} tensor(s), got {}",
+                        #num_fields,
+                        tensors.len(),
+                    );
+
+                    let tensors = &mut ::std::iter::IntoIterator::into_iter(tensors);
+                    ::std::result::Result::Ok(Self {
+                        #(
+                            #field_names: ::std::convert::TryInto::try_into(
+                                ::std::iter::Iterator::next(tensors)
+                                    .expect("the length should have been checked"),
+                            )?,
+                        )*
+                    })
+                }
+            }
+        })
+    }
+}
+
+fn struct_field_names(data: &Data) -> syn::Result<Vec<&syn::Ident>> {
+    let fields = match data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => fields,
+        Data::Struct(DataStruct { fields, .. }) => {
+            return Err(syn::Error::new(fields.span(), "expect named fields"));
+        }
+        Data::Enum(DataEnum { enum_token, .. }) => {
+            return Err(syn::Error::new(enum_token.span(), "expected an enum"));
+        }
+        Data::Union(DataUnion { union_token, .. }) => {
+            return Err(syn::Error::new(union_token.span(), "expected an enum"));
+        }
+    };
+
+    Ok(fields
+        .named
+        .iter()
+        .map(|Field { ident, .. }| ident.as_ref().expect("should be named"))
+        .collect())
+}
