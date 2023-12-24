aa803881d05dcdb408b3434607713927fc2e7d9c
Ryo Yamashita <qryxip@gmail.com>
2023-12-24 19:19:03 +0900

    `to_wav`を移動

--- crates/voicevox_core/src/synthesizer.rs
+++ crates/voicevox_core/src/synthesizer.rs
@@ -381,52 +381,6 @@ pub(crate) mod blocking {
                     pitch,
                 )
             }
-
-            fn to_wav(wave: &[f32], audio_query: &AudioQueryModel) -> Vec<u8> {
-                let volume_scale = *audio_query.volume_scale();
-                let output_stereo = *audio_query.output_stereo();
-                let output_sampling_rate = *audio_query.output_sampling_rate();
-
-                // TODO: 44.1kHzなどの対応
-
-                let num_channels: u16 = if output_stereo { 2 } else { 1 };
-                let bit_depth: u16 = 16;
-                let repeat_count: u32 =
-                    (output_sampling_rate / DEFAULT_SAMPLING_RATE) * num_channels as u32;
-                let block_size: u16 = bit_depth * num_channels / 8;
-
-                let bytes_size = wave.len() as u32 * repeat_count * 2;
-                let wave_size = bytes_size + 44;
-
-                let buf: Vec<u8> = Vec::with_capacity(wave_size as usize);
-                let mut cur = Cursor::new(buf);
-
-                cur.write_all("RIFF".as_bytes()).unwrap();
-                cur.write_all(&(wave_size - 8).to_le_bytes()).unwrap();
-                cur.write_all("WAVEfmt ".as_bytes()).unwrap();
-                cur.write_all(&16_u32.to_le_bytes()).unwrap(); // fmt header length
-                cur.write_all(&1_u16.to_le_bytes()).unwrap(); //linear PCM
-                cur.write_all(&num_channels.to_le_bytes()).unwrap();
-                cur.write_all(&output_sampling_rate.to_le_bytes()).unwrap();
-
-                let block_rate = output_sampling_rate * block_size as u32;
-
-                cur.write_all(&block_rate.to_le_bytes()).unwrap();
-                cur.write_all(&block_size.to_le_bytes()).unwrap();
-                cur.write_all(&bit_depth.to_le_bytes()).unwrap();
-                cur.write_all("data".as_bytes()).unwrap();
-                cur.write_all(&bytes_size.to_le_bytes()).unwrap();
-
-                for value in wave {
-                    let v = (value * volume_scale).clamp(-1., 1.);
-                    let data = (v * 0x7fff as f32) as i16;
-                    for _ in 0..repeat_count {
-                        cur.write_all(&data.to_le_bytes()).unwrap();
-                    }
-                }
-
-                cur.into_inner()
-            }
         }
 
         /// AquesTalk風記法からAccentPhrase (アクセント句)の配列を生成する。
@@ -1206,6 +1160,52 @@ pub(crate) mod blocking {
                 Some(kana),
             )
         }
+    }
+
+    fn to_wav(wave: &[f32], audio_query: &AudioQueryModel) -> Vec<u8> {
+        let volume_scale = *audio_query.volume_scale();
+        let output_stereo = *audio_query.output_stereo();
+        let output_sampling_rate = *audio_query.output_sampling_rate();
+
+        // TODO: 44.1kHzなどの対応
+
+        let num_channels: u16 = if output_stereo { 2 } else { 1 };
+        let bit_depth: u16 = 16;
+        let repeat_count: u32 =
+            (output_sampling_rate / DEFAULT_SAMPLING_RATE) * num_channels as u32;
+        let block_size: u16 = bit_depth * num_channels / 8;
+
+        let bytes_size = wave.len() as u32 * repeat_count * 2;
+        let wave_size = bytes_size + 44;
+
+        let buf: Vec<u8> = Vec::with_capacity(wave_size as usize);
+        let mut cur = Cursor::new(buf);
+
+        cur.write_all("RIFF".as_bytes()).unwrap();
+        cur.write_all(&(wave_size - 8).to_le_bytes()).unwrap();
+        cur.write_all("WAVEfmt ".as_bytes()).unwrap();
+        cur.write_all(&16_u32.to_le_bytes()).unwrap(); // fmt header length
+        cur.write_all(&1_u16.to_le_bytes()).unwrap(); //linear PCM
+        cur.write_all(&num_channels.to_le_bytes()).unwrap();
+        cur.write_all(&output_sampling_rate.to_le_bytes()).unwrap();
+
+        let block_rate = output_sampling_rate * block_size as u32;
+
+        cur.write_all(&block_rate.to_le_bytes()).unwrap();
+        cur.write_all(&block_size.to_le_bytes()).unwrap();
+        cur.write_all(&bit_depth.to_le_bytes()).unwrap();
+        cur.write_all("data".as_bytes()).unwrap();
+        cur.write_all(&bytes_size.to_le_bytes()).unwrap();
+
+        for value in wave {
+            let v = (value * volume_scale).clamp(-1., 1.);
+            let data = (v * 0x7fff as f32) as i16;
+            for _ in 0..repeat_count {
+                cur.write_all(&data.to_le_bytes()).unwrap();
+            }
+        }
+
+        cur.into_inner()
     }
 }
 
