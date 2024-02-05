1f542f9ade8fb96a1c938829cf6360dc99bf3069
nekomimimi <okt666@yahoo.co.jp>
2024-02-05 23:27:50 +0900

    重複ロジックを削除した。細部を見直した。

--- build_util/ios_xcframework.bash
+++ build_util/ios_xcframework.bash
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
-set -e
-# - name: Create aarch64 Framework
-mkdir -p "Framework-aarch64/voicevox_core.framework/Headers"
-cp -vr "crates/voicevox_core_c_api/xcframework/Frameworks/aarch64/" "Framework-aarch64/"
-cp -v "artifact/voicevox_core-aarch64-apple-ios/voicevox_core.h" \
-    "Framework-aarch64/voicevox_core.framework/Headers/voicevox_core.h"
-cp -v "artifact/voicevox_core-aarch64-apple-ios/libvoicevox_core.dylib" \
-    "Framework-aarch64/voicevox_core.framework/voicevox_core"
+set -eu
 
-# - name: Change aarch64 @rpath
-# 自身への@rpathを変更
-install_name_tool -id "@rpath/voicevox_core.framework/voicevox_core" "Framework-aarch64/voicevox_core.framework/voicevox_core"
-
-# 依存ライブラリを調べると@rpath/libonnxruntime.A.BB.C.dylibの文字列があるので、
-# これを@rpath/onnxruntime.framework/onnxruntimeに変更したい。
-
-# dylib名取得
+echo "* Get original onnixruntime file name from rpath"
 output=$(otool -L "Framework-aarch64/voicevox_core.framework/voicevox_core")
 matched_line=$(echo "$output" | grep "@rpath" | grep "libonnxruntime")
 if [[ $matched_line ]]; then
@@ -31,12 +17,29 @@ else
     echo "$output"
     exit 1
 fi
-echo "onnx dylib file name: $dylib_string"
+echo "Origial onnx dylib file name: $dylib_string"
 
+echo "* Create aarch64 Framework"
+mkdir -p "Framework-aarch64/voicevox_core.framework/Headers"
+cp -vr "crates/voicevox_core_c_api/xcframework/Frameworks/aarch64/" \
+    "Framework-aarch64/"
+cp -v "artifact/voicevox_core-aarch64-apple-ios/voicevox_core.h" \
+    "Framework-aarch64/voicevox_core.framework/Headers/voicevox_core.h"
+cp -v "artifact/voicevox_core-aarch64-apple-ios/libvoicevox_core.dylib" \
+    "Framework-aarch64/voicevox_core.framework/voicevox_core"
+
+echo "* Change aarch64 @rpath"
+# 自身への@rpathを変更
+install_name_tool -id "@rpath/voicevox_core.framework/voicevox_core" \
+    "Framework-aarch64/voicevox_core.framework/voicevox_core"
+# 依存ライブラリを調べると@rpath/libonnxruntime.A.BB.C.dylibの文字列があるので、
+# これを@rpath/onnxruntime.framework/onnxruntimeに変更したい。
 # onnxruntimeへの@rpathを変更
-install_name_tool -change "@rpath/$dylib_string" "@rpath/onnxruntime.framework/onnxruntime" Framework-aarch64/voicevox_core.framework/voicevox_core
+install_name_tool -change "@rpath/$dylib_string" \
+    "@rpath/onnxruntime.framework/onnxruntime" \
+    "Framework-aarch64/voicevox_core.framework/voicevox_core"
 
-# - name: Create sim Framework 
+echo "* Create sim Framework"
 mkdir -p "Framework-sim/voicevox_core.framework/Headers"
 cp -vr "crates/voicevox_core_c_api/xcframework/Frameworks/sim/" "Framework-sim/"
 cp -v "artifact/voicevox_core-aarch64-apple-ios/voicevox_core.h" \
@@ -45,32 +48,16 @@ lipo -create "artifact/voicevox_core-x86_64-apple-ios/libvoicevox_core.dylib" \
     "artifact/voicevox_core-aarch64-apple-ios-sim/libvoicevox_core.dylib" \
     -output "Framework-sim/voicevox_core.framework/voicevox_core"
 
-# - name: Change sim @rpath   
+echo "* Change sim @rpath"   
 # 自身への@rpathを変更
-install_name_tool -id "@rpath/voicevox_core.framework/voicevox_core" "Framework-sim/voicevox_core.framework/voicevox_core"
-
-# dylib名取得(方針はaarch64同じ)
-output=$(otool -L "Framework-sim/voicevox_core.framework/voicevox_core")
-matched_line=$(echo "$output" | grep "@rpath" | grep "libonnxruntime")
-if [[ $matched_line ]]; then
-    if [[ $matched_line =~ (@rpath/([^ ]+\.dylib)) ]]; then
-        dylib_string=${BASH_REMATCH[2]}
-    else
-        echo "Expected pattern not found in the matched line"
-        echo "$output"
-        exit 1
-    fi
-else
-    echo "No line containing '@rpath' and 'libonnxruntime' found"
-    echo "$output"
-    exit 1
-fi
-echo "onnx dylib file name: $dylib_string"
-
+install_name_tool -id "@rpath/voicevox_core.framework/voicevox_core" \
+    "Framework-sim/voicevox_core.framework/voicevox_core"
 # onnxruntimeへの@rpathを変更
-install_name_tool -change "@rpath/$dylib_string" "@rpath/onnxruntime.framework/onnxruntime" Framework-sim/voicevox_core.framework/voicevox_core
+install_name_tool -change "@rpath/$dylib_string" \
+    "@rpath/onnxruntime.framework/onnxruntime" \
+    "Framework-sim/voicevox_core.framework/voicevox_core"
 
-#      - name: Create XCFramework
+echo "* Create XCFramework"
 mkdir -p "artifact/${ASSET_NAME}"
 xcodebuild -create-xcframework \
     -framework "Framework-sim/voicevox_core.framework" \
