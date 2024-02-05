72fad0d387f12d4f8e5c71c95327e47c0c506b25
nekomimimi <okt666@yahoo.co.jp>
2024-02-05 16:42:25 +0900

    文法的なミスを修正

--- build_util/ios_xcframework.bash
+++ build_util/ios_xcframework.bash
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
+set -e
 # - name: Create aarch64 Framework
 mkdir -p "Framework-aarch64/voicevox_core.framework/Headers"
-cp -vr crates/voicevox_core_c_api/xcframework/Frameworks/aarch64/ Framework-aarch64/
-cp -v artifact/voicevox_core-aarch64-apple-ios/voicevox_core.h \
-    Framework-aarch64/voicevox_core.framework/Headers/voicevox_core.h
-cp -v artifact/voicevox_core-aarch64-apple-ios/libvoicevox_core.dylib \
-    Framework-aarch64/voicevox_core.framework/voicevox_core
+cp -vr "crates/voicevox_core_c_api/xcframework/Frameworks/aarch64/" "Framework-aarch64/"
+cp -v "artifact/voicevox_core-aarch64-apple-ios/voicevox_core.h" \
+    "Framework-aarch64/voicevox_core.framework/Headers/voicevox_core.h"
+cp -v "artifact/voicevox_core-aarch64-apple-ios/libvoicevox_core.dylib" \
+    "Framework-aarch64/voicevox_core.framework/voicevox_core"
 
 # - name: Change aarch64 @rpath
 # 自身への@rpathを変更
-install_name_tool -id @rpath/voicevox_core.framework/voicevox_core Framework-aarch64/voicevox_core.framework/voicevox_core
+install_name_tool -id "@rpath/voicevox_core.framework/voicevox_core" "Framework-aarch64/voicevox_core.framework/voicevox_core"
 
 # 依存ライブラリを調べると@rpath/libonnxruntime.A.BB.C.dylibの文字列があるので、
 # これを@rpath/onnxruntime.framework/onnxruntimeに変更したい。
 
 # dylib名取得
-output=$(otool -L Framework-aarch64/voicevox_core.framework/voicevox_core)
+output=$(otool -L "Framework-aarch64/voicevox_core.framework/voicevox_core")
 matched_line=$(echo "$output" | grep "@rpath" | grep "libonnxruntime")
 if [[ $matched_line ]]; then
     if [[ $matched_line =~ (@rpath/([^ ]+\.dylib)) ]]; then
@@ -37,19 +38,19 @@ install_name_tool -change "@rpath/$dylib_string" "@rpath/onnxruntime.framework/o
 
 # - name: Create sim Framework 
 mkdir -p "Framework-sim/voicevox_core.framework/Headers"
-cp -vr crates/voicevox_core_c_api/xcframework/Frameworks/sim/ Framework-sim/
-cp -v artifact/voicevox_core-aarch64-apple-ios/voicevox_core.h \
-    Framework-sim/voicevox_core.framework/Headers/voicevox_core.h
-lipo -create artifact/voicevox_core-x86_64-apple-ios/libvoicevox_core.dylib \
-    artifact/voicevox_core-aarch64-apple-ios-sim/libvoicevox_core.dylib \
-    -output Framework-sim/voicevox_core.framework/voicevox_core
+cp -vr "crates/voicevox_core_c_api/xcframework/Frameworks/sim/" "Framework-sim/"
+cp -v "artifact/voicevox_core-aarch64-apple-ios/voicevox_core.h" \
+    "Framework-sim/voicevox_core.framework/Headers/voicevox_core.h"
+lipo -create "artifact/voicevox_core-x86_64-apple-ios/libvoicevox_core.dylib" \
+    "artifact/voicevox_core-aarch64-apple-ios-sim/libvoicevox_core.dylib" \
+    -output "Framework-sim/voicevox_core.framework/voicevox_core"
 
 # - name: Change sim @rpath   
 # 自身への@rpathを変更
-install_name_tool -id @rpath/voicevox_core.framework/voicevox_core Framework-sim/voicevox_core.framework/voicevox_core
+install_name_tool -id "@rpath/voicevox_core.framework/voicevox_core" "Framework-sim/voicevox_core.framework/voicevox_core"
 
 # dylib名取得(方針はaarch64同じ)
-output=$(otool -L Framework-sim/voicevox_core.framework/voicevox_core)
+output=$(otool -L "Framework-sim/voicevox_core.framework/voicevox_core")
 matched_line=$(echo "$output" | grep "@rpath" | grep "libonnxruntime")
 if [[ $matched_line ]]; then
     if [[ $matched_line =~ (@rpath/([^ ]+\.dylib)) ]]; then
@@ -70,8 +71,8 @@ echo "onnx dylib file name: $dylib_string"
 install_name_tool -change "@rpath/$dylib_string" "@rpath/onnxruntime.framework/onnxruntime" Framework-sim/voicevox_core.framework/voicevox_core
 
 #      - name: Create XCFramework
-mkdir -p "artifact/${{ env.ASSET_NAME }}"
+mkdir -p "artifact/${ASSET_NAME}"
 xcodebuild -create-xcframework \
     -framework "Framework-sim/voicevox_core.framework" \
     -framework "Framework-aarch64/voicevox_core.framework" \
-    -output "artifact/${{ env.ASSET_NAME }}/voicevox_core.xcframework"
+    -output "artifact/${ASSET_NAME}/voicevox_core.xcframework"
