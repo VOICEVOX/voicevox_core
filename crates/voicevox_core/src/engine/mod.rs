57efe0208dcb987e024945657933c639c25aa3c4
femshima <49227365+femshima@users.noreply.github.com>
2024-02-09 08:17:35 +0900

    fmt

--- crates/voicevox_core/src/engine/mod.rs
+++ crates/voicevox_core/src/engine/mod.rs
@@ -6,7 +6,7 @@ mod mora_list;
 pub(crate) mod open_jtalk;
 
 pub(crate) use self::acoustic_feature_extractor::OjtPhoneme;
-pub(crate) use self::full_context_label::{FullContextLabelError, extract_full_context_label};
+pub(crate) use self::full_context_label::{extract_full_context_label, FullContextLabelError};
 pub(crate) use self::kana_parser::{create_kana, parse_kana, KanaParseError};
 pub use self::model::{AccentPhraseModel, AudioQueryModel, MoraModel};
 pub(crate) use self::mora_list::mora2text;
