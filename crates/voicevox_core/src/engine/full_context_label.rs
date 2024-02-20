950d5904a2dd2024da63631a012a45910ed8fe0a
femshima <49227365+femshima@users.noreply.github.com>
2024-02-20 14:53:57 +0900

    copy chunk_by from Rust

--- crates/voicevox_core/src/engine/full_context_label.rs
+++ crates/voicevox_core/src/engine/full_context_label.rs
@@ -65,17 +65,8 @@ fn generate_accent_phrases(
             .unwrap_or(0),
     );
 
-    let split = utterance.split_by(|label| {
-        (
-            label
-                .breath_group_curr
-                .as_ref()
-                .map(|bg| bg.breath_group_position_backward),
-            label
-                .accent_phrase_curr
-                .as_ref()
-                .map(|ap| ap.accent_phrase_position_forward),
-        )
+    let split = utterance.chunk_by(|a, b| {
+        a.breath_group_curr == b.breath_group_curr && a.accent_phrase_curr == b.accent_phrase_curr
     });
     for labels in split {
         let moras = generate_moras(labels)?;
@@ -124,8 +115,7 @@ fn generate_accent_phrases(
 fn generate_moras(accent_phrase: &[Label]) -> std::result::Result<Vec<MoraModel>, ErrorKind> {
     let mut moras = Vec::with_capacity(accent_phrase.len());
 
-    let split =
-        accent_phrase.split_by(|label| label.mora.as_ref().map(|mora| mora.position_forward));
+    let split = accent_phrase.chunk_by(|a, b| a.mora == b.mora);
     for labels in split {
         let mut label_iter = labels.iter().filter(|label| label.mora.is_some());
         match (label_iter.next(), label_iter.next(), label_iter.next()) {
@@ -177,35 +167,104 @@ pub fn mora_to_text(consonant: Option<&str>, vowel: &str) -> String {
     engine::mora2text(&mora_text).to_string()
 }
 
-#[easy_ext::ext]
-impl<T> [T] {
-    /// Returns an iterator over subslices grouped by return value of `pred`.
-    fn split_by<'a, F, V>(&'a self, mut pred: F) -> impl Iterator<Item = &'a [T]>
-    where
-        T: 'a,
-        F: FnMut(&'a T) -> V,
-        V: Eq,
-    {
-        use itertools::Itertools as _;
-        self.iter().enumerate().batching(move |it| {
-            let (i, k) = it.next()?;
-            let v = pred(k);
-            let n = it.take_while_ref(|(_, x)| pred(x) == v).count();
-            Some(&self[i..=i + n])
-        })
+// FIXME: Remove `chunk_by` module after Rust 1.77.0 is released as stable.
+use chunk_by::*;
+mod chunk_by {
+    // Implementations in this module were copied from
+    // [Rust](https://github.com/rust-lang/rust/blob/746a58d4359786e4aebb372a30829706fa5a968f/library/core/src/slice/iter.rs).
+
+    // MIT License Notice
+
+    // Permission is hereby granted, free of charge, to any
+    // person obtaining a copy of this software and associated
+    // documentation files (the "Software"), to deal in the
+    // Software without restriction, including without
+    // limitation the rights to use, copy, modify, merge,
+    // publish, distribute, sublicense, and/or sell copies of
+    // the Software, and to permit persons to whom the Software
+    // is furnished to do so, subject to the following
+    // conditions:
+    //
+    // The above copyright notice and this permission notice
+    // shall be included in all copies or substantial portions
+    // of the Software.
+    //
+    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+    // ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+    // TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+    // PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+    // SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    // CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+    // IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    // DEALINGS IN THE SOFTWARE.
+
+    pub struct ChunkBy<'a, T, P> {
+        slice: &'a [T],
+        predicate: P,
     }
-}
+    impl<'a, T, P> ChunkBy<'a, T, P> {
+        pub(super) fn new(slice: &'a [T], predicate: P) -> Self {
+            ChunkBy { slice, predicate }
+        }
+    }
+    impl<'a, T, P> Iterator for ChunkBy<'a, T, P>
+    where
+        P: FnMut(&T, &T) -> bool,
+    {
+        type Item = &'a [T];
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        #[inline]
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.slice.is_empty() {
+                None
+            } else {
+                let mut len = 1;
+                let mut iter = self.slice.windows(2);
+                while let Some([l, r]) = iter.next() {
+                    if (self.predicate)(l, r) {
+                        len += 1
+                    } else {
+                        break;
+                    }
+                }
+                let (head, tail) = self.slice.split_at(len);
+                self.slice = tail;
+                Some(head)
+            }
+        }
 
-    #[test]
-    fn split_by() {
-        let mut split = [0, 0, 1, 1, 1, -5].split_by(|n| n);
-        assert_eq!(split.next(), Some([0, 0].as_slice()));
-        assert_eq!(split.next(), Some([1, 1, 1].as_slice()));
-        assert_eq!(split.next(), Some([-5].as_slice()));
-        assert_eq!(split.next(), None);
+        #[inline]
+        fn size_hint(&self) -> (usize, Option<usize>) {
+            if self.slice.is_empty() {
+                (0, Some(0))
+            } else {
+                (1, Some(self.slice.len()))
+            }
+        }
+    }
+
+    #[easy_ext::ext(TChunkBy)]
+    impl<T> [T] {
+        pub fn chunk_by<F>(&self, pred: F) -> ChunkBy<'_, T, F>
+        where
+            F: FnMut(&T, &T) -> bool,
+        {
+            ChunkBy::new(self, pred)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::TChunkBy;
+
+        #[test]
+        fn split_by() {
+            let mut split = [0, 0, 1, 1, 1, -5].chunk_by(|a, b| a == b);
+            assert_eq!(split.next(), Some([0, 0].as_slice()));
+            assert_eq!(split.next(), Some([1, 1, 1].as_slice()));
+            assert_eq!(split.next(), Some([-5].as_slice()));
+            assert_eq!(split.next(), None);
+        }
     }
 }
