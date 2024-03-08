04eee9afc50a917e66d2561b4eac0e7d124373d5
takejohn <105504345+takejohn@users.noreply.github.com>
2024-03-08 22:10:03 +0900

    元のソースに対応してファイル分け

---
+++ crates/voicevox_core_node_api/src/devices.rs
@@ -0,0 +1,41 @@
+#![deny(clippy::all)]
+
+use napi::{Error, Result};
+
+use voicevox_core::SupportedDevices;
+
+#[napi(js_name = "SupportedDevices")]
+pub struct JsSupportedDevices {
+    handle: SupportedDevices,
+}
+
+#[napi]
+impl JsSupportedDevices {
+    #[napi(factory)]
+    pub fn create() -> Result<Self> {
+        match SupportedDevices::create() {
+            Ok(val) => Ok(JsSupportedDevices { handle: val }),
+            Err(err) => Err(Error::from_reason(err.to_string())),
+        }
+    }
+
+    #[napi(getter)]
+    pub fn cpu(&self) -> bool {
+        self.handle.cpu
+    }
+
+    #[napi(getter)]
+    pub fn cuda(&self) -> bool {
+        self.handle.cuda
+    }
+
+    #[napi(getter)]
+    pub fn dml(&self) -> bool {
+        self.handle.dml
+    }
+
+    #[napi]
+    pub fn to_json(&self) -> serde_json::Value {
+        self.handle.to_json()
+    }
+}
