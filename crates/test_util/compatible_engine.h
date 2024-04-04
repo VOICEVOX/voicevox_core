68086fe4e334c5385e2d597b879c3dc421330b61
Ryo Yamashita <qryxip@gmail.com>
2024-04-05 02:50:14 +0900

    compatible_engine.hから、不要かつ今存在しないはずの関数を削除

--- crates/test_util/compatible_engine.h
+++ crates/test_util/compatible_engine.h
@@ -28,16 +28,3 @@ bool decode_forward(int64_t length, int64_t phoneme_size, float *f0,
                     float *phoneme, int64_t *speaker_id, float *output);
 
 const char *last_error_message();
-
-VoicevoxResultCode voicevox_load_openjtalk_dict(const char *dict_path);
-
-VoicevoxResultCode voicevox_tts(const char *text, int64_t speaker_id,
-                                int *output_binary_size, uint8_t **output_wav);
-
-VoicevoxResultCode voicevox_tts_from_kana(const char *text, int64_t speaker_id,
-                                          int *output_binary_size,
-                                          uint8_t **output_wav);
-
-void voicevox_wav_free(uint8_t *wav);
-
-const char *voicevox_error_result_to_message(VoicevoxResultCode result_code);
