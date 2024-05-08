bae5bead53ee6c2d56cc1cff1e4b981a57649353
Ryo Yamashita <qryxip@gmail.com>
2024-05-09 02:42:05 +0900

    chore: sample.vvmをcrates/test_util/data/model下に構成するようにする

--- crates/voicevox_core_c_api/tests/e2e/testcases/synthesizer_new_output_json.rs
+++ crates/voicevox_core_c_api/tests/e2e/testcases/synthesizer_new_output_json.rs
@@ -4,7 +4,6 @@ use std::{
 };
 
 use assert_cmd::assert::AssertResult;
-use cstr::cstr;
 use libloading::Library;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
@@ -55,7 +54,7 @@ impl assert_cdylib::TestCase for TestCase {
         let model = {
             let mut model = MaybeUninit::uninit();
             assert_ok(lib.voicevox_voice_model_new_from_path(
-                cstr!("../../model/sample.vvm").as_ptr(),
+                c_api::SAMPLE_VOICE_MODEL_FILE_PATH.as_ptr(),
                 model.as_mut_ptr(),
             ));
             model.assume_init()
