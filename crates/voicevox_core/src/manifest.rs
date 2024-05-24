be8d0c065fb8a0da442062e605d3b7b583398ecc
Ryo Yamashita <qryxip@gmail.com>
2024-05-25 02:40:40 +0900

    change: `manifest_version`を`vvm_format_version`に (#794)
    * change: `manifest_version`を`vvm_format_version`に
    
    * エラーメッセージを変更
    
    * エラーメッセージを変更
    
    * 文字列ではなく整数にする

--- crates/voicevox_core/src/manifest.rs
+++ crates/voicevox_core/src/manifest.rs
@@ -1,20 +1,49 @@
-use std::{collections::BTreeMap, fmt::Display, sync::Arc};
+use std::{
+    collections::BTreeMap,
+    fmt::{self, Display},
+    sync::Arc,
+};
 
 use derive_getters::Getters;
 use derive_more::Deref;
 use derive_new::new;
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Deserializer, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
 
 use crate::{StyleId, VoiceModelId};
 
-pub type RawManifestVersion = String;
-#[derive(Deserialize, Clone, Debug, PartialEq, new)]
-pub struct ManifestVersion(RawManifestVersion);
+#[derive(Clone)]
+struct FormatVersionV1;
 
-impl Display for ManifestVersion {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+impl<'de> Deserialize<'de> for FormatVersionV1 {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        return deserializer.deserialize_any(Visitor);
+
+        struct Visitor;
+
+        impl<'de> de::Visitor<'de> for Visitor {
+            type Value = FormatVersionV1;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                formatter.write_str("an unsigned integer")
+            }
+
+            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                match v {
+                    1 => Ok(FormatVersionV1),
+                    v => Err(E::custom(format!(
+                        "未知の形式です（`vvm_format_version={v}`）。新しいバージョンのVOICEVOX \
+                         COREであれば対応しているかもしれません",
+                    ))),
+                }
+            }
+        }
     }
 }
 
@@ -39,7 +68,7 @@ impl Display for InnerVoiceId {
 #[derive(Deserialize, Getters, Clone)]
 pub struct Manifest {
     #[allow(dead_code)]
-    manifest_version: ManifestVersion,
+    vvm_format_version: FormatVersionV1,
     pub(crate) id: VoiceModelId,
     metas_filename: String,
     #[serde(flatten)]
@@ -66,3 +95,38 @@ pub(crate) struct TalkManifest {
 pub(crate) struct StyleIdToInnerVoiceId(
     #[serde_as(as = "Arc<BTreeMap<DisplayFromStr, _>>")] Arc<BTreeMap<StyleId, InnerVoiceId>>,
 );
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Deref;
+
+    use rstest::rstest;
+    use serde::Deserialize;
+
+    use super::FormatVersionV1;
+
+    #[rstest]
+    #[case("{\"vvm_format_version\":1}", Ok(()))]
+    #[case(
+        "{\"vvm_format_version\":2}",
+        Err(
+            "未知の形式です（`vvm_format_version=2`）。新しいバージョンのVOICEVOX COREであれば対応\
+             しているかもしれません at line 1 column 23",
+        )
+    )]
+    fn vvm_format_version_works(
+        #[case] input: &str,
+        #[case] expected: Result<(), &str>,
+    ) -> anyhow::Result<()> {
+        let actual = serde_json::from_str::<ManifestPart>(input).map_err(|e| e.to_string());
+        let actual = actual.as_ref().map(|_| ()).map_err(Deref::deref);
+        assert_eq!(expected, actual);
+        return Ok(());
+
+        #[derive(Deserialize)]
+        struct ManifestPart {
+            #[allow(dead_code)]
+            vvm_format_version: FormatVersionV1,
+        }
+    }
+}
