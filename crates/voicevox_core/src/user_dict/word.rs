8f18cef7049c254a201fa8c19146a0e4a2c683c0
Ryo Yamashita <qryxip@gmail.com>
2024-07-26 20:52:21 +0900

    Fix Clippy lints

--- crates/voicevox_core/src/user_dict/word.rs
+++ crates/voicevox_core/src/user_dict/word.rs
@@ -212,8 +212,10 @@ fn calculate_mora_count(pronunciation: &str, accent_type: usize) -> InvalidWordR
 /// 一部の種類の文字を、全角文字に置き換える。
 ///
 /// 具体的には
+///
 /// - "!"から"~"までの範囲の文字(数字やアルファベット)は、対応する全角文字に
 /// - " "などの目に見えない文字は、まとめて全角スペース(0x3000)に
+///
 /// 変換する。
 pub(crate) fn to_zenkaku(surface: &str) -> String {
     // 元実装：https://github.com/VOICEVOX/voicevox/blob/69898f5dd001d28d4de355a25766acb0e0833ec2/src/components/DictionaryManageDialog.vue#L379-L387
