5c01e9c44b626d9f181462fd02975dc2d8983f82
Ryo Yamashita <qryxip@gmail.com>
2024-12-13 05:00:21 +0900

    refactor: VVMマニフェストで`#[serde(flatten)]`を活用 (#891)
    #825 を見越したリファクタ。

--- crates/voicevox_core_macros/src/lib.rs
+++ crates/voicevox_core_macros/src/lib.rs
@@ -3,7 +3,6 @@
 mod extract;
 mod inference_domain;
 mod inference_domains;
-mod manifest;
 
 use syn::parse_macro_input;
 
@@ -101,38 +100,6 @@ pub fn derive_inference_output_signature(
 ) -> proc_macro::TokenStream {
     let input = &parse_macro_input!(input);
     from_syn(inference_domain::derive_inference_output_signature(input))
-}
-
-/// 構造体のフィールドを取得できる`std::ops::Index`の実装を導出する。
-///
-/// # Example
-///
-/// ```
-/// use macros::IndexForFields;
-///
-/// #[derive(IndexForFields)]
-/// #[index_for_fields(TalkOperation)]
-/// pub(crate) struct TalkManifest {
-///     #[index_for_fields(TalkOperation::PredictDuration)]
-///     pub(crate) predict_duration_filename: Arc<str>,
-///
-///     #[index_for_fields(TalkOperation::PredictIntonation)]
-///     pub(crate) predict_intonation_filename: Arc<str>,
-///
-///     #[index_for_fields(TalkOperation::GenerateFullIntermediate)]
-///     pub(crate) generate_full_intermediate_filename: Arc<str>,
-///
-///     #[index_for_fields(TalkOperation::RenderAudioSegment)]
-///     pub(crate) render_audio_segment_filename: Arc<str>,
-///
-///     // …
-/// }
-/// ```
-#[cfg(not(doctest))]
-#[proc_macro_derive(IndexForFields, attributes(index_for_fields))]
-pub fn derive_index_for_fields(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = &parse_macro_input!(input);
-    from_syn(manifest::derive_index_for_fields(input))
 }
 
 /// # Example
