c3d4e61d3cc63cd41629542844ed9e458a31c7e1
Ryo Yamashita <qryxip@gmail.com>
2025-10-21 12:52:14 +0900

    refactor: 半角→全角変換処理とモーラ数カウント処理を移動 (#1176)
    VOICEVOX/voicevox_engine#1679 および VOICEVOX/voicevox_engine#1695 と同
    じようにコードの移動を行う。モーラ数のカウントについては #1161 に必要で
    もある。
    
    移動先のモジュールはENGINEでは`voicevox_engine.utility.text_utility`だっ
    たが、COREには"utility"のような名前の置き場は無いため別の名前を考えた。
    
    上記の通り`text_utility`という名前を直接使ってもおらず、ENGINEの著作権が
    及ぶかはかなり微妙ではあるが一応、以下の1名の許諾のもと #874 にのっとり
    MITライセンスとしてライセンスする。
    
    - @tarepan
    
    (VOICEVOX/voicevox_engine#1695 については、code suggestionの分は本PRとは
    無関係)
    
    Co-authored-by: tarepan <tarepan5884@gmail.com>

---
+++ crates/voicevox_core/src/engine/talk/text/hankaku_zenkaku.rs
@@ -0,0 +1,40 @@
+use std::sync::LazyLock;
+
+use regex::Regex;
+
+/// 一部の種類の文字を、全角文字に置き換える。
+///
+/// 具体的には
+///
+/// - "!"から"~"までの範囲の文字(数字やアルファベット)は、対応する全角文字に
+/// - " "などの目に見えない文字は、まとめて全角スペース(0x3000)に
+///
+/// 変換する。
+pub(crate) fn to_zenkaku(s: &str) -> String {
+    // 元実装：https://github.com/VOICEVOX/voicevox/blob/69898f5dd001d28d4de355a25766acb0e0833ec2/src/components/DictionaryManageDialog.vue#L379-L387
+
+    static SPACE_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\p{Z}").unwrap());
+
+    SPACE_REGEX
+        .replace_all(s, "\u{3000}")
+        .chars()
+        .map(|c| match u32::from(c) {
+            i @ 0x21..=0x7e => char::from_u32(0xfee0 + i).unwrap_or(c),
+            _ => c,
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("abcdefg", "ａｂｃｄｅｆｇ")]
+    #[case("あいうえお", "あいうえお")]
+    #[case("a_b_c_d_e_f_g", "ａ＿ｂ＿ｃ＿ｄ＿ｅ＿ｆ＿ｇ")]
+    #[case("a b c d e f g", "ａ　ｂ　ｃ　ｄ　ｅ　ｆ　ｇ")]
+    fn to_zenkaku_works(#[case] before: &str, #[case] after: &str) {
+        assert_eq!(super::to_zenkaku(before), after);
+    }
+}
