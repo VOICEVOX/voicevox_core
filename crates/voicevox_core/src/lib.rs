1eb3bb2623c77d3ee5cedc1e8cfce71f6f7f9581
Ryo Yamashita <qryxip@gmail.com>
2025-12-13 03:12:42 +0900

    feat(rust): `Phoneme`と`Sil`と`SamplingRate`をパブリックに

--- crates/voicevox_core/src/lib.rs
+++ crates/voicevox_core/src/lib.rs
@@ -315,9 +315,12 @@ pub use self::{
         metas::{CharacterMeta, CharacterVersion, StyleId, StyleMeta, StyleType, VoiceModelMeta},
         voice_model::VoiceModelId,
     },
-    engine::talk::{
-        user_dict::{UserDictWord, UserDictWordBuilder, UserDictWordType},
-        AccentPhrase, AudioQuery, Mora,
+    engine::{
+        talk::{
+            user_dict::{UserDictWord, UserDictWordBuilder, UserDictWordType},
+            AccentPhrase, AudioQuery, Mora,
+        },
+        Phoneme, SamplingRate, Sil,
     },
     error::{Error, ErrorKind},
     result::Result,
