88063c397f05667354d3b1f2c6ec85417c7a31f2
Ryo Yamashita <qryxip@gmail.com>
2026-09-09 21:23:12 +0900

    足す

--- crates/voicevox_core_c_api/src/lib.rs
+++ crates/voicevox_core_c_api/src/lib.rs
@@ -2215,6 +2215,7 @@ pub unsafe extern "C" fn voicevox_json_free(json: *mut c_char) {
 ///     - ::voicevox_synthesizer_tts
 ///     - ::voicevox_synthesizer_tts_from_kana
 ///     - ::voicevox_synthesizer_frame_synthesis
+///     - ::voicevox_wav_from_s16le
 /// - `wav`がヌルポインタでも ::voicevox_empty_bytes でもないならば、<a href="#voicevox-core-safety">読み込みと書き込みについて有効</a>でなければならない。
 /// - `wav`がヌルポインタでも ::voicevox_empty_bytes でもないならば、以後<b>ダングリングポインタ</b>(_dangling pointer_)として扱われなくてはならない。
 /// }
