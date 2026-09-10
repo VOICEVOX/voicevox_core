5822ac160308096b5cb76aa59e30b6f83b47d7e3
Yosshi999 <Yosshi999@users.noreply.github.com>
2026-09-10 21:25:45 +0900

    apply suggestion

--- crates/voicevox_core_java_api/src/audio_feature.rs
+++ crates/voicevox_core_java_api/src/audio_feature.rs
@@ -4,20 +4,20 @@ use jni::{
     sys::{jdouble, jlong},
 };
 use voicevox_core::AudioFeature;
-use std::sync::Arc;
 
 use crate::common::throw_if_err;
 
-
 // SAFETY: voicevox_core_java_apiを構成するライブラリの中に、これと同名のシンボルは存在しない
 #[unsafe(no_mangle)]
-extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsFrameRate(_: JNIEnv<'_>) -> jdouble {
-    voicevox_core::AudioFeature::FRAME_RATE
+extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsFrameRate(
+    _: JNIEnv<'_>,
+) -> jdouble {
+    AudioFeature::FRAME_RATE
 }
 
 // SAFETY: voicevox_core_java_apiを構成するライブラリの中に、これと同名のシンボルは存在しない
 #[unsafe(no_mangle)]
-extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsGetFrameLength<'local>(
+unsafe extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsGetFrameLength<'local>(
     env: JNIEnv<'local>,
     this: JObject<'local>,
 ) -> jlong {
@@ -27,20 +27,19 @@ extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsGetFrameLength<
             // SAFETY:
             // - The safety contract must be upheld by the caller.
             // - `jp.hiroshiba.voicevoxcore.AudioFeature.handle` must correspond to
-            //   `Arc<voicevox_core::AudioFeature>`.
-            env.get_rust_field::<_, _, Arc<AudioFeature>>(&this, "handle")
-        }?
-        .clone();
+            //   `voicevox_core::AudioFeature`.
+            env.get_rust_field::<_, _, AudioFeature>(&this, "handle")
+        }?;
 
         Ok(internal.frame_length())
     })
     .try_into()
-    .expect("FrameLength is not considered to be so large")
+    .expect("`frame_length` is not considered to be so large")
 }
 
 // SAFETY: voicevox_core_java_apiを構成するライブラリの中に、これと同名のシンボルは存在しない
 #[unsafe(no_mangle)]
-extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsDrop<'local>(
+unsafe extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsDrop<'local>(
     env: JNIEnv<'local>,
     this: JObject<'local>,
 ) {
@@ -49,8 +48,8 @@ extern "system" fn Java_jp_hiroshiba_voicevoxcore_AudioFeature_rsDrop<'local>(
             // SAFETY:
             // - The safety contract must be upheld by the caller.
             // - `jp.hiroshiba.voicevoxcore.AudioFeature.handle` must correspond to
-            //   `Arc<voicevox_core::AudioFeature>`.
-            env.take_rust_field::<_, _, Arc<AudioFeature>>(&this, "handle")
+            //   `voicevox_core::AudioFeature`.
+            env.take_rust_field::<_, _, AudioFeature>(&this, "handle")
         }?;
         Ok(())
     })
