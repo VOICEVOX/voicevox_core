1ead1174ad33200a653b217271e0c825e6ee6612
Ryo Yamashita <qryxip@gmail.com>
2026-09-26 11:33:05 +0900

    間違えた！

--- crates/voicevox_core_python_api/python/voicevox_core/_python/__init__.py
+++ crates/voicevox_core_python_api/python/voicevox_core/_python/__init__.py
@@ -522,7 +522,7 @@ class AudioQuery:
            として算出する。ここで ``round_ties_even`` は
            |audio-query-frame-length-round-ties-even|_ とする。この関数はPythonの
            |audio-query-frame-length-round|_ や |audio-query-frame-length-numpy-round|_
-           と同様、IEEE 754の ``roundToIntegralTowardNegative``
+           と同様、IEEE 754の ``roundToIntegralTiesToEven``
            演算を行う。また乗算と除算も32-bit浮動小数点数上で行う。
         3. 各フレーム長を足し合わせる。
 
